5dcdc405de4bb6ccea1bb437223f6c6fb3322866
Pavan Bobade <pavanbobade01@gmail.com>
2026-07-05 14:30:55 +0530

    Remove secret_keys.dart from repository

--- lib/secret_keys.dart
+++
@@ -1,9 +0,0 @@
-// lib/secret_keys.dart
-class SecretKeys {
-  // DO NOT COMMIT this file to GitHub.
-  // Add values locally before running the app.
-  // Example:
-  // static const googleMapsApiKey = 'AIza....';
-  static const String googleMapsApiKey = 'AIzaSyDtJ-_vVHdjNK5yoguE7WPJN2gR9pX-ccM';
-
-}
